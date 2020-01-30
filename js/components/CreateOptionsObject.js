81b641ebf1141fc109e3344b8bd1ce9b488644fa
Aaron Southammavong <aaroneight07@gmail.com>
2020-01-30 13:13:54 -0800

    added CreateOptionsObject.js to handle flat-image-objects

---
+++ js/components/CreateOptionsObject.js
@@ -0,0 +1,75 @@
+import React, { Component, useState } from "react";
+import { ScrollView, View, StyleSheet, Image } from "react-native";
+import { connect } from "react-redux";
+import { navigate } from "../redux/render/render.action";
+import {
+  Container,
+  Header,
+  Content,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button
+} from "native-base";
+
+const CreateOptionsObject = props => {
+  return (
+    <Container style={{ width: "100%", height: "100%" }}>
+      <Header>
+        <Left>
+          <Button
+            hasText
+            transparent
+            onPress={() => {
+              props.navigate("REACT_NATIVE_HOME");
+            }}
+          >
+            <Text>Back</Text>
+          </Button>
+        </Left>
+        <Body />
+        <Right />
+      </Header>
+      <Content>
+        <View style={{ marginTop: 50 }}>
+          <Button
+            vertical
+            onPress={() => {
+              props.navigate("CAMERA_PAGE_OBJECT");
+            }}
+          >
+            <Text>Take a Photo</Text>
+          </Button>
+          <Button
+            vertical
+            onPress={() => {
+              props.navigate("IMAGE_PICKER_PAGE_OBJECT");
+            }}
+          >
+            <Text>Upload Image</Text>
+          </Button>
+        </View>
+      </Content>
+    </Container>
+  );
+};
+
+const localStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 15
+    },
+    profileImg: {
+      marginTop: 30,
+      width: 60,
+      height: 60,
+      borderRadius: 10
+    }
+  });
+  const mapDispatchToProps = dispatch => {
+    return {
+      navigate: render => dispatch(navigate(render))
+    };
+  };
+  export default connect(null, mapDispatchToProps)(CreateOptionsObject);
