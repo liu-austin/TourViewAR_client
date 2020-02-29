72fbd3ce6afe5828f33823f074454025322dacea
liu-austin <austinliu279@gmail.com>
2020-02-29 12:41:05 -0800

    makes ios ipa available to build and begins setting up for android debug apk

--- android/app/src/main/java/com/tourviewarclient/MainApplication.java
+++ android/app/src/main/java/com/tourviewarclient/MainApplication.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+
+import com.viromedia.bridge.ReactViroPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
@@ -24,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf(BuildConfig.VR_PLATFORM))
           new ImagePickerPackage()
       );
     }
